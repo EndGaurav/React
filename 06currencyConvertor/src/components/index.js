dbc5556702455be15926cebcd6dd4e78443dc8e5
Gaurav Singh Jethuri <gauravsinghjethuri2002@gmail.com>
2023-10-23 13:24:18 +0530

    Currency Converter project, custom hook.

---
+++ 06currencyConvertor/src/components/index.js
@@ -0,0 +1,4 @@
+// it's a best practice to import all the components in this index.js file and from here just export the compenent one by one.
+import InputBox from "./InputBox";
+
+export {InputBox}
